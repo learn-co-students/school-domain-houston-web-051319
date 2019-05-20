b52859cb16e8235f6c6555898464db1e0663ca4e
Maruf <marufbinawal@gmail.com>
2019-05-20 09:23:39 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+    def initialize()
+
+   def roster['students']=[]
+    roster['students'] << "array_student"
+    roster => {"students"=>["array_student"]}
+    end
+
+     def grade = roster.each do |grade|
+        grade = students.array_student
+     end
+
+    
+end
